4931f6f3996709823cb26505d76d331743d7ed8e
Viktoriia <viktoriia.i@code-care.pro>
2022-12-11 15:57:18 +0200

    Set general configs

--- next.config.js
+++ next.config.js
@@ -1,6 +1,16 @@
 /** @type {import('next').NextConfig} */
+
+const { withFrameworkConfig } = require('./framework/common/config');
+
 const nextConfig = {
   reactStrictMode: true,
+  framework: {
+    name: "shopify"
+  },
+  i18n: {
+    locales: ["en-US", "es"],
+    defaultLocale: "en-US",
+  }
 }
 
-module.exports = nextConfig
+module.exports = withFrameworkConfig(nextConfig)
