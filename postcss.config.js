acfd440b4322c6bf1db79828ea212aa2b13dbc2e
Viktoriia <viktoriia.i@code-care.pro>
2022-12-12 20:49:48 +0200

    Set styles

---
+++ postcss.config.js
@@ -0,0 +1,14 @@
+module.exports = {
+  plugins: [
+    "tailwindcss",
+    "autoprefixer",
+  ]
+}
+
+
+// Just for testing
+// module.exports = {
+//   plugins: [
+//     require("tailwindcss")
+//   ]
+// }
